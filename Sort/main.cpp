273c4abac9dbbcc949c1c05fbbfb56799b321977
Booker Sebastian <Booker@mbp.lan>
2016-03-19 23:56:54 +0800

    Initial Commit

---
+++ Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sort
+//
+//  Created by Booker Sebastian on 3/19/16.
+//  Copyright © 2016 Ryan Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
